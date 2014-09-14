59b47227f60a303f4466864ca35a22f84f421e33
zhengyajun <zhengyajun@baidu.com>
2014-09-14 22:57:17 +0800

    add achartengine for wave drawing

--- src/com/heart/heart_rate_monitor/HeartRateMonitor.java
+++ src/com/heart/heart_rate_monitor/HeartRateMonitor.java
@@ -3,12 +3,22 @@ package com.heart.heart_rate_monitor;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
 import android.annotation.SuppressLint;
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint.Align;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
 import android.hardware.Camera.PreviewCallback;
@@ -21,6 +31,7 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -35,7 +46,19 @@ import com.health.surfaceviews.Constants;
  * 
  * @author zheng yajun <yajun0601@gmail.com>
  */
+@SuppressLint("NewApi")
 public class HeartRateMonitor extends Activity {
+	private String title = "Signal Strength"; 
+	private static XYSeries series;  
+	private static XYMultipleSeriesDataset mDataset; 
+	private static GraphicalView chart;  
+	private static XYMultipleSeriesRenderer renderer; 
+	private static int addX = -1;
+	private static int addY; 
+	static int[] xv = new int[100]; 
+	static int[] yv = new int[100]; 
+	
+	
 	private static int waitSeconds = 0;
     private static final String TAG = "HeartRateMonitor";
     private static final AtomicBoolean processing = new AtomicBoolean(false);
@@ -79,21 +102,7 @@ public class HeartRateMonitor extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        onOffToggleBtn = (ToggleButton) findViewById(R.id.onOffToggleBtn);
-        
-        onOffToggleBtn.setOnClickListener(new OnClickListener() {      
-            public void onClick(View v) {    
-                if (onOffToggleBtn.isChecked()) {   
-                	wakeLock.acquire();
-                    openCamera();
-                }   
-                else {       
-                    wakeLock.release();
-                    close_camera();
-                }      
-            }  
-          });  
+        setContentView(R.layout.main);    
         preview = (SurfaceView) findViewById(R.id.preview);
         previewHolder = preview.getHolder();
         previewHolder.addCallback(surfaceCallback);
@@ -106,15 +115,94 @@ public class HeartRateMonitor extends Activity {
 
 		surfaceHolder = view.getHolder();
 		
-		
         image = findViewById(R.id.image);
         text = (TextView) findViewById(R.id.text);
 
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         wakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "DoNotDimScreen");       
         //setPreviewFpsRange
-    }
+        
+      //这个类用来放置曲线上的所有点，是一个点的集合，根据这些点画出曲线 
+        series = new XYSeries(title);  
+      //创建一个数据集的实例，这个数据集将被用来创建图表 
+        mDataset = new XYMultipleSeriesDataset();  
+      //将点集添加到这个数据集中
+        mDataset.addSeries(series);  
+      //以下都是曲线的样式和属性等等的设置，renderer相当于一个用来给图表做渲染的句柄 
+      int color = Color.RED; 
+      PointStyle style = PointStyle.CIRCLE; 
+      renderer = buildRenderer(color, style, true); 
+      LinearLayout layout = (LinearLayout) findViewById(R.id.chart);
+    //设置好图表的样式
+      setChartSettings(renderer, "X", "Y", 0, 100, 20, 100, Color.WHITE, Color.WHITE); 
+      // 生成图表
+      chart = ChartFactory.getLineChartView(this, mDataset, renderer); 
+      //将图表添加到布局中去
+      layout.addView(chart, new LayoutParams(LayoutParams.FILL_PARENT, 
+      LayoutParams.FILL_PARENT)); 
 
+    }
+    @SuppressLint("NewApi")
+	protected XYMultipleSeriesRenderer buildRenderer(int color, PointStyle style, boolean fill) { 
+    	XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();  
+    	//设置图表中曲线本身的样式，包括颜色、点的大小以及线的粗细等 
+    	XYSeriesRenderer r = new XYSeriesRenderer(); 
+    	r.setColor(color); r.setPointStyle(style); 
+    	r.setFillPoints(fill); r.setLineWidth(3); 
+    	renderer.addSeriesRenderer(r);  
+    	return renderer; 
+    	}
+    protected void setChartSettings(XYMultipleSeriesRenderer renderer, String xTitle, String yTitle, 
+    		double xMin, double xMax, double yMin, double yMax, int axesColor, int labelsColor) { 
+    	//有关对图表的渲染可参看api文档 
+    	renderer.setChartTitle(title); 
+    	renderer.setXTitle(xTitle); 
+    	renderer.setYTitle(yTitle); 
+    	renderer.setXAxisMin(xMin); 
+    	renderer.setXAxisMax(xMax); 
+    	renderer.setYAxisMin(yMin); 
+    	renderer.setYAxisMax(yMax); 
+    	renderer.setAxesColor(axesColor); 
+    	renderer.setLabelsColor(labelsColor); 
+    	renderer.setShowGrid(true); 
+    	renderer.setGridColor(Color.GREEN); 
+    	renderer.setXLabels(20); 
+    	renderer.setYLabels(10); 
+    	renderer.setXTitle("Time"); 
+    	renderer.setYTitle("dBm"); 
+    	renderer.setYLabelsAlign(Align.RIGHT); 
+    	renderer.setPointSize((float) 2); 
+    	renderer.setShowLegend(false); 
+    }
+    private static void updateChart(int yy) {  
+    	//设置好下一个需要增加的节点 
+    	addX = 0; 
+    	addY = yy; //移除数据集中旧的点集
+    	mDataset.removeSeries(series); 
+    	//判断当前点集中到底有多少点，因为屏幕总共只能容纳100个，所以当点数超过100时，长度永远是100 
+    	int length = series.getItemCount(); 
+    	if (length > 100) { 
+    		length = 100;  
+    	}
+    	//将旧的点集中x和y的数值取出来放入backup中，并且将x的值加1，造成曲线向右平移的效果 
+    	for (int i = 0; i < length; i++) { 
+    		xv[i] = (int) series.getX(i) + 1;
+    		yv[i] = (int) series.getY(i); 
+    		} 
+    	//点集先清空，为了做成新的点集而准备 
+    	series.clear();  
+    	//将新产生的点首先加入到点集中，然后在循环体中将坐标变换后的一系列点都重新加入到点集中 
+    	//这里可以试验一下把顺序颠倒过来是什么效果，即先运行循环体，再添加新产生的点 
+    	series.add(addX, addY);   
+    	for (int k = 0; k < length; k++) { 
+    		series.add(xv[k], yv[k]); 
+    		} 
+    	//在数据集中添加新的点集 
+    	mDataset.addSeries(series);  
+    	//视图更新，没有这一步，曲线不会呈现动态 
+    	//如果在非UI主线程中，需要调用postInvalidate()，具体参考api chart.invalidate(); 
+    	chart.invalidate();
+    }
     /**
      * {@inheritDoc}
      */
@@ -173,6 +261,7 @@ public class HeartRateMonitor extends Activity {
     	return imgAvg;
     }
     private static PreviewCallback previewCallback = new PreviewCallback() {
+    	float times = 1;
     	 public void drawWave(){
          	Canvas c;
          	c = null;
@@ -194,7 +283,7 @@ public class HeartRateMonitor extends Activity {
             Camera.Size size = cam.getParameters().getPreviewSize();
             if (size == null) throw new NullPointerException();
 
-            if (!processing.compareAndSet(false, true)) return;
+            if (!processing.compareAndSet(false, true)) return;        	
             /*
             Camera.Parameters camParas = camera.getParameters();
             int range[] = {3,4};
@@ -226,9 +315,20 @@ public class HeartRateMonitor extends Activity {
 
             int rollingAverage = (averageArrayCnt > 0) ? (averageArrayAvg / averageArrayCnt) : 0;
             if(rollingAverage !=0){
-            	int middleOfView = (view.getHeight() - view.getTop())/2;
-                 view.yy = imgAvg/(rollingAverage /middleOfView + 1);
-                //Log.i(TAG,"Y:"+view.yy + " H:" + view.getHeight() + " avg:"+ rollingAverage +" img: " + imgAvg);
+            	
+            	int middleOfView = (chart.getHeight() - chart.getTop())/2;
+            	int yy = imgAvg/(rollingAverage /middleOfView + 1);
+            	if( yy / times > 100 ){
+            		times += 0.5;            		
+            	}
+            	if(yy/times < 20){
+            		times -= 0.1;
+            	}
+                updateChart((int)(yy/times));
+                
+            	int middleOfView1 = (view.getHeight() - view.getTop())/2;
+                 view.yy = imgAvg/(rollingAverage /middleOfView1 + 1);
+                Log.i(TAG,"Y:"+view.yy + " H:" + chart.getHeight() + " avg:"+ rollingAverage +" img: " + imgAvg);
 
             	drawWave();
             }
